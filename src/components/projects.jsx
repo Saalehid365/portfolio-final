9d018669576cac1ae99729bc38d32cbb0f6c34da
Saaleh Nelson <100734424+Saalehid365@users.noreply.github.com>
2023-07-06 07:45:46 +0100

    project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,6 +4,9 @@ import project2 from "../assests/traditionalwellness.jpg";
 import project3 from "../assests/kacrm.jpg";
 import project4 from "../assests/salafibookmarks.png";
 import homeedVid from "../assests/homeedVid.mov";
+import masjid from "../assests/masjid.png";
+import woman from "../assests/womanhood.png";
+import bookmark from "../assests/salafibook.png";
 import { FaHtml5, FaCss3, FaReact, FaJs, FaGithub } from "react-icons/fa";
 
 const Projects = () => {
@@ -20,6 +23,129 @@ const Projects = () => {
           Latest
           <span className="border-b-2 p-2 border-yellow-500">projects</span>
         </p>
+      </div>
+      <div className="flex flex-row-reverse justify-around w-screen px-28 mb-48 md:flex md:flex-col md:items-center md:px-0">
+        <div className="md:flex md:items-center md:justify-center">
+          <img
+            className="h-96 w-5/6 rounded-xl "
+            src={woman}
+            alt="project1"
+          ></img>
+        </div>
+        <div className="flex flex-col justify-center w-2/5 md:w-full md:flex md:items-center">
+          <p className="text-3xl text-gray-400 md:mt-8">
+            Womanhood and Joy Blog Website
+          </p>
+          <p className="w-4/5 text-lg mt-3 text-gray-500">
+            A Website for a client who needed a blog website for their lifestyle
+          </p>
+          <div className="flex items-center pt-4 justify-between w-3/4 text-gray-500 md:w-1/2 sm:w-full sm:flex sm:justify-evenly">
+            <p>Made with</p>
+            <div className="bg-neutral-800 w-72 h-12 flex items-center justify-evenly  text-gray-500 text-4xl rounded-md sm:w-52">
+              <FaHtml5 />
+              <FaCss3 />
+              <FaJs />
+              <FaReact />
+            </div>
+          </div>
+          <div className="flex w-72 justify-between mt-12">
+            <button
+              onClick={toggleModal1}
+              className="bg-teal-700 border-solid w-32 rounded-md text-gray-400 text-2xl hover:bg-teal-600 hover:text-gray-300"
+            >
+              Demo
+            </button>
+            <a
+              href="https://github.com/Saalehid365/homeschoolTrippy"
+              className="h-12 w-32 flex  items-center justify-evenly border border-solid border-gray-400 rounded-md text-gray-400 text-2xl hover:bg-neutral-600 hover:text-gray-300"
+            >
+              <FaGithub />
+              Code
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* --- project1 --- */}
+      <div className="flex justify-around w-screen px-28 mb-48 md:flex md:flex-col md:items-center md:px-0">
+        <div className="md:flex md:items-center md:justify-center">
+          <img
+            className="h-96 w-5/6 rounded-xl "
+            src={masjid}
+            alt="project1"
+          ></img>
+        </div>
+        <div className="flex flex-col justify-center w-2/5 md:w-full md:flex md:items-center">
+          <p className="text-3xl text-gray-400 md:mt-8">AlMusa3ada Website</p>
+          <p className="w-4/5 text-lg mt-3 text-gray-500">
+            A customers website for an appeal to help fund a new mosque in Egypt
+          </p>
+          <div className="flex items-center pt-4 justify-between w-3/4 text-gray-500 md:w-1/2 sm:w-full sm:flex sm:justify-evenly">
+            <p>Made with</p>
+            <div className="bg-neutral-800 w-72 h-12 flex items-center justify-evenly  text-gray-500 text-4xl rounded-md sm:w-52">
+              <FaHtml5 />
+              <FaCss3 />
+              <FaJs />
+              <FaReact />
+            </div>
+          </div>
+          <div className="flex w-72 justify-between mt-12">
+            <button
+              onClick={toggleModal1}
+              className="bg-teal-700 border-solid w-32 rounded-md text-gray-400 text-2xl hover:bg-teal-600 hover:text-gray-300"
+            >
+              Demo
+            </button>
+            <a
+              href="https://github.com/Saalehid365/homeschoolTrippy"
+              className="h-12 w-32 flex  items-center justify-evenly border border-solid border-gray-400 rounded-md text-gray-400 text-2xl hover:bg-neutral-600 hover:text-gray-300"
+            >
+              <FaGithub />
+              Code
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* --- project2 --- */}
+      <div className="flex flex-row-reverse justify-around w-screen px-28 mb-48 md:flex md:flex-col md:items-center md:px-0">
+        <div className="md:flex md:items-center md:justify-center">
+          <img
+            className="h-96 w-5/6 rounded-xl "
+            src={bookmark}
+            alt="project1"
+          ></img>
+        </div>
+        <div className="flex flex-col justify-center w-2/5 md:w-full md:flex md:items-center">
+          <p className="text-3xl text-gray-400 md:mt-8">
+            Salafibookmarks E-commerce Website
+          </p>
+          <p className="w-4/5 text-lg mt-3 text-gray-500">
+            A Website for a client who sells islamic books and accesories
+          </p>
+          <div className="flex items-center pt-4 justify-between w-3/4 text-gray-500 md:w-1/2 sm:w-full sm:flex sm:justify-evenly">
+            <p>Made with</p>
+            <div className="bg-neutral-800 w-72 h-12 flex items-center justify-evenly  text-gray-500 text-4xl rounded-md sm:w-52">
+              <FaHtml5 />
+              <FaCss3 />
+              <FaJs />
+              <FaReact />
+            </div>
+          </div>
+          <div className="flex w-72 justify-between mt-12">
+            <button
+              onClick={toggleModal1}
+              className="bg-teal-700 border-solid w-32 rounded-md text-gray-400 text-2xl hover:bg-teal-600 hover:text-gray-300"
+            >
+              Demo
+            </button>
+            <a
+              href="https://github.com/Saalehid365/homeschoolTrippy"
+              className="h-12 w-32 flex  items-center justify-evenly border border-solid border-gray-400 rounded-md text-gray-400 text-2xl hover:bg-neutral-600 hover:text-gray-300"
+            >
+              <FaGithub />
+              Code
+            </a>
+          </div>
+        </div>
       </div>
       {/* --- project1 --- */}
       <div className="flex justify-around w-screen px-28 mb-48 md:flex md:flex-col md:items-center md:px-0">
